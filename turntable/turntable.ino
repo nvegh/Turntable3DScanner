c5935a8a1ad790b45e7dd58457f632467580401b
nvegh <mr.norbert.vegh@gmail.com>
2018-06-30 00:08:33 +0200

    Long press -> blink control

--- turntable/turntable.ino
+++ turntable/turntable.ino
@@ -9,7 +9,19 @@ typedef enum {
     BTN_STATE_PRESSED
 } ButtonState;
 
+typedef enum {
+    STANDBY,
+    FUNCTION1,
+    FUNCTION2
+} ButtonInput;
+
 ButtonState btnStatus = BTN_STATE_RELEASED;
+ButtonInput btnInput = STANDBY;
+unsigned long btnPressed_time;
+
+#define blinqSequenceArr 7
+#define wait 1250
+unsigned long blinqSequence[] = {wait /*OFF*/, 100/*ON*/, wait /*OFF[1]*/, 100 /*ON*/, 100 /*OFF*/, 100 /*ON*/, wait /*OFF[2]*/};
 
 void setup() {
   pinMode(shutterPin,OUTPUT);
@@ -18,33 +30,67 @@ void setup() {
   pinMode(buttonPin,INPUT_PULLUP);
 
   pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin,HIGH);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  int btnVal = digitalRead(buttonPin);
-      
-  digitalWrite(ledPin, !btnVal);
-
+    int btnVal = digitalRead(buttonPin);
+  
     switch(btnStatus)
     {
     case BTN_STATE_PRESSED:
-        // Handle button release
-        if(btnVal == HIGH)
+        
+        ProcessButtonInput();
+       
+        if(btnVal == HIGH) // Handle button release
         {
-            triggerTwoPush();
+            if (btnInput == FUNCTION2) triggerTwoPush();
+            
+            Serial.println(btnInput);
+            digitalWrite(ledPin,HIGH);
             btnStatus = BTN_STATE_RELEASED;
         }
 
         break;
 
     case BTN_STATE_RELEASED:
-        // Handle button press
-        if(btnVal == LOW)
+        
+        if(btnVal == LOW) // Handle button press
         {
             btnStatus = BTN_STATE_PRESSED;
+            
+            btnPressed_time = millis();
+            digitalWrite(ledPin,LOW);
         }
         break;
     }
+}
+
+void ProcessButtonInput(){
+  unsigned long timeNow = millis();
+  unsigned long sum = btnPressed_time;  
+  
+  for(int i=1; i<blinqSequenceArr; i++)
+  {
+    sum += blinqSequence[i-1];
+    
+    if (sum < timeNow && sum + blinqSequence[i] > timeNow)
+    {
+      int j = i-1;
+      //[i]-th time interval
+      digitalWrite(ledPin, !((j)%(int)2));
+      if (i == 2) btnInput = FUNCTION1;
+      if (i == 6) btnInput = FUNCTION2;
+      break;
+    }
+    if (i==blinqSequenceArr-1) {
+      digitalWrite(ledPin, timeNow > sum + wait);
+      btnInput = STANDBY;
+    }
+  }
+
 }
 
 
