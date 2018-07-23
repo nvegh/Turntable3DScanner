3aee8bb0cd933a0bb7301fc214a52d53ee53df78
nvegh <mr.norbert.vegh@gmail.com>
2018-07-24 01:49:55 +0200

    Cancelliable func, rewrite

--- turntable/turntable.ino
+++ turntable/turntable.ino
@@ -13,65 +13,72 @@ typedef enum {
     BTN_STATE_PRESSED
 } ButtonState;
 
+ButtonState btnStatus = BTN_STATE_RELEASED;
+unsigned long btnPressed_time = 0;
+unsigned long btnReleased_time = 0;
+unsigned long funcTime = 0;
+
 typedef enum {
     STANDBY,
-    FUNCTION1,
-    FUNCTION2
+    CANCELLED,
+    FUNCTION1_RUNNING,
+    FUNCTION2_RUNNING
 } ButtonInput;
-
-ButtonState btnStatus = BTN_STATE_RELEASED;
 ButtonInput btnInput = STANDBY;
-unsigned long btnPressed_time;
-unsigned long event_time;
-
-typedef enum {
-    ON,
-    OFF,
-    SELECTING,
-    WORKING
-} LEDState;
-LEDState ledStatus = ON;
-
+ButtonInput _tmpInput = NULL;
 
 #define blinqSequenceArr 7
 #define wait 1250
 unsigned long blinqSequence[] = {wait /*OFF*/, 150/*ON*/, wait /*OFF[1]*/, 150 /*ON*/, 150 /*OFF*/, 150 /*ON*/, wait /*OFF[2]*/};
-bool job = false;
 
-void setup()
-{
+int funcVal;
+
+void setup() {
+  pinMode(buttonPin,INPUT_PULLUP);
   pinMode(shutterPin,OUTPUT);
   digitalWrite(shutterPin,LOW);
-
-  pinMode(buttonPin,INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
   
-  Timer1.initialize(5000);
+  Timer1.initialize(10000);
   Timer1.attachInterrupt(processIO);
+
+  Serial.begin(9600);
 }
- 
-void processIO()
-{
-    switch(ledStatus)
+
+void processIO() {
+    
+
+    //BUTTON status
+    int btnVal = digitalRead(buttonPin);
+    switch(btnStatus)
     {
-      case ON:
-          digitalWrite(ledPin, 1);
-          break;
-
-        case OFF:
-          digitalWrite(ledPin, 0);
-          break;
-
-        case WORKING:
+    case BTN_STATE_PRESSED:
+        if(btnVal == HIGH) // Handle button release
         {
-            int x = (millis()-event_time) % 1000;
-            if (x <= 500) { analogWrite(ledPin, map(x, 0, 500, 0, 200)); }
-            else
-            {  analogWrite(ledPin, map((x-1000)*-1, 0, 500, 0, 200)); }
+            btnStatus = BTN_STATE_RELEASED;
+            btnReleased_time = millis();
         }
         break;
-          
-      case SELECTING:{
+
+    case BTN_STATE_RELEASED:
+        if(btnVal == LOW) // Handle button press
+        {
+            btnStatus = BTN_STATE_PRESSED;
+            btnPressed_time = millis();
+        }
+        break;
+    }
+
+
+  //BEHAVIOUR LOGIC
+   if (btnInput != STANDBY && btnStatus ==  BTN_STATE_PRESSED && millis()-btnPressed_time > 1500) btnInput =  CANCELLED;
+   if (btnInput == CANCELLED && btnStatus ==  BTN_STATE_RELEASED) btnInput = STANDBY;
+   if (_tmpInput != NULL && btnStatus ==  BTN_STATE_RELEASED) { btnInput = _tmpInput; _tmpInput = NULL; funcTime = btnReleased_time; funcVal=-1;}
+
+
+    // *** LED ***
+    //SELECTING
+    if (btnStatus ==  BTN_STATE_PRESSED && btnInput == STANDBY) {
             unsigned long timeNow = millis();
             unsigned long sum = btnPressed_time;   
             
@@ -83,91 +90,71 @@ void processIO()
                 int j = i-1;
                 //[i]-th time interval
                 digitalWrite(ledPin, !((j)%(int)2));
-                if (i == 2) btnInput = FUNCTION1;
-                if (i == 6) btnInput = FUNCTION2;
+                if (i == 2) _tmpInput = FUNCTION1_RUNNING;
+                if (i == 6) _tmpInput = FUNCTION2_RUNNING;
                 break;
               }
               if (i==blinqSequenceArr-1) {
                 digitalWrite(ledPin, timeNow > sum + wait);
-                btnInput = STANDBY;
+                _tmpInput = STANDBY;
               }
             }
-      }
-          break;
     }
-}
- 
-void loop()
-{
-    delay(50);
-    int btnVal = digitalRead(buttonPin);
-  
-    switch(btnStatus)
-    {
-    case BTN_STATE_PRESSED:
 
+  //WORKING
+   else if ( btnInput != STANDBY ) {
+        unsigned long timeNow = millis();
         
-        if (job == false) {   SetLedState(SELECTING); }
-       
-        if(btnVal == HIGH) // Handle button release
-        {
-          if (job == false ){
-            SetLedState(WORKING);
-            job = true;
-            
-            switch (btnInput){
+        if (btnStatus ==  BTN_STATE_PRESSED && timeNow-btnPressed_time < 1500 || btnStatus ==  BTN_STATE_RELEASED){
+          
+          if (timeNow > funcTime + 500){
+          int x = (timeNow-funcTime + 500 ) % 1000;
+          if (x <= 500)  analogWrite(ledPin, map(x, 0, 500, 0, 200)); 
+          else analogWrite(ledPin, map((x-1000)*-1, 0, 500, 0, 200)); 
+          } else analogWrite(ledPin, 0);
+        }
+        else //CANCELLING
+        {  
+          if ( timeNow-btnPressed_time > 1500 && timeNow-btnPressed_time < 2000) digitalWrite(ledPin, 0);
+          if ( timeNow-btnPressed_time > 2000) { digitalWrite(ledPin, 1); };
+        }
+   }
 
-              case STANDBY:
-                  SetLedState(ON);
-                  job = false;
-              break;
+   //STANDBY
+    else if (btnStatus ==  BTN_STATE_RELEASED && btnInput == STANDBY) { digitalWrite(ledPin, 1);}
+
+}
+
+
+void loop() {
      
-              case FUNCTION1:
-                  Turn();
-              break;
-              
-              case FUNCTION2:
-                delay(1000);
-                for (int i=0; i <= 5; i++){
-                  TurnOneStep();
-                }
-                btnInput = STANDBY;
-                SetLedState(ON);
-                job = false;
-              break;
+      switch (btnInput){
 
+        case STANDBY:
+            analogWrite(DC_CW, 0);
+            analogWrite(DC_CCW, 0);
+        break;
 
-            }
-          }
-          else {  //button pressed during operation
-              job = false;
-              btnInput = STANDBY;
-             SetLedState(ON);
-             analogWrite(DC_CCW, 0);
-            
-            }
-            //Serial.println(btnInput);
-            btnStatus = BTN_STATE_RELEASED;
+        case CANCELLED:
+            analogWrite(DC_CW, 0);
+            analogWrite(DC_CCW, 0);
+        break;
+        
+        case FUNCTION2_RUNNING:
+            Turn();
+        break;
+        
+        case FUNCTION1_RUNNING: {
+          unsigned int timeNow = millis();
+          int i = (timeNow-funcTime) / (int)2000;
+          int m = (timeNow-funcTime) % 2000;
+          if (m<60) analogWrite(DC_CCW, 100); else analogWrite(DC_CCW, 0);
+          if (i!=funcVal) { funcVal=i;  triggerCHDKTwoPush(); Serial.println("shoot!");}
+          if (i>=3) btnInput = STANDBY;
         }
         break;
 
-    case BTN_STATE_RELEASED:
-        
-        if(btnVal == LOW) // Handle button press
-        {
-              btnStatus = BTN_STATE_PRESSED;
-              btnPressed_time = millis();
-              SetLedState(OFF);
-            }
-  
-        break;
-    }
-}
-
-
-void SetLedState(LEDState newState){
-    ledStatus = newState;
-    event_time = millis();
+        }
 }
 
 
